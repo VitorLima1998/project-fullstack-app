481d65116d1e4510eb6b87c269c18d3bf6738949
VitorLima1998 <vitor.xlr8@gmail.com>
2023-09-09 21:43:54 -0400

    index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { Home } from './components/Home/index.jsx';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Home />);
+root.render(<App />);
