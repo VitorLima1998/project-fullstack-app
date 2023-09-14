7f31f442d5d73d908bb4abaf81b49996ee61e4cd
VitorLima1998 <vitor.xlr8@gmail.com>
2023-09-14 16:07:54 -0400

    feat: create handleLogout

--- src/components/Products/index.jsx
+++ src/components/Products/index.jsx
@@ -3,13 +3,18 @@ import { loadProducts } from '../../utils/loadProducts';
 import { Button } from '../Button';
 import { ProdCard } from '../ProductCard';
 import { Input } from '../Input';
+import { logout } from '../../services/authService';
+import { toast } from 'react-toastify';
+import { useNavigate } from 'react-router-dom';
+import { FaRegPlusSquare, FaRunning } from 'react-icons/fa';
+
 import './styles.scss';
 
 export const Products = () => {
   const [products, setProducts] = useState([]);
   const [allProducts, setAllProducts] = useState([]);
   const [page, setPage] = useState(0);
-  const [productPerPage] = useState(8);
+  const [productPerPage] = useState(10);
   const [searchValue, setSearchValue] = useState('');
 
   // -------------------------------------------------------------------------------------
@@ -22,12 +27,25 @@ export const Products = () => {
 
   // -------------------------------------------------------------------------------------
 
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    const success = await logout();
+    if (success) {
+      toast.success('Logout successfully!');
+      navigate('/');
+    }
+  };
+
+  // -------------------------------------------------------------------------------------
+
   const handleLoadProducts = useCallback(async (page, productPerPage) => {
     const products = await loadProducts();
     setProducts(products.data.slice(page, productPerPage));
     setAllProducts(products.data);
   }, []);
 
+  console.log(products);
   // -------------------------------------------------------------------------------------
 
   const loadMoreProducts = () => {
@@ -55,13 +73,18 @@ export const Products = () => {
 
   return (
     <section className='container'>
-      <Input
-        type='text'
-        name='txtSearch'
-        placeholder='Search...'
-        value={searchValue}
-        onChange={handleSearch}
-      />
+      <div className='nav'>
+        <Input
+          type='text'
+          name='txtSearch'
+          placeholder='Search...'
+          value={searchValue}
+          onChange={handleSearch}
+          id='inputOutline'
+        />
+        <Button text='Add Products' icon={<FaRegPlusSquare />} />
+        <Button text='Logout' action={handleLogout} icon={<FaRunning />} />
+      </div>
       <div className='products'>
         {filteredProducts.map((product) => (
           <ProdCard key={product.id} product={product} />
